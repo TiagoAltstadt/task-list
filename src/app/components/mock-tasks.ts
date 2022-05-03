fc3bd99e197f8a36ae2a4f095f89d044ce8e38c1
TiagoAltstadt <tiagoaltstadt@gmail.com>
2022-05-03 16:02:12 -0300

    Pseudo base de datos y modelo tareas

---
+++ src/app/components/mock-tasks.ts
@@ -0,0 +1,35 @@
+import {Task} from "./Task";
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Task One",
+        day: "15/05",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Task Two",
+        day: "15/05",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Task Three",
+        day: "15/05",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Task Four",
+        day: "15/05",
+        reminder: false
+    },
+    {
+        id: 1,
+        text: "Task One",
+        day: "15/05",
+        reminder: true
+    },
+
+]
